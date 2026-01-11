9b88e90652a54aba7b94db8328b2121cb685e0dd
joseph horvath <josephhorvath00@gmail.com>
2026-01-10 22:22:56 -0500

    Added Year Released to Song model

--- music-manager-start.Data/Models/Song.cs
+++ music-manager-start.Data/Models/Song.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace music_manager_starter.Data.Models
 {
     public sealed class Song
     {
         public Guid Id { get; set; }
+
         public string Title { get; set; } = string.Empty;
+
         public string Artist { get; set; } = string.Empty;
+
         public string Album { get; set; } = string.Empty;
+
         public string Genre { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Optional year the song was released.
+        /// Nullable to support legacy data.
+        /// </summary>
+        public int? YearReleased { get; set; }
     }
 }
