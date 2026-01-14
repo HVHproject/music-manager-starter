cf0d63b51e0b51045e6b62c246587c417e867738
joseph horvath <josephhorvath00@gmail.com>
2026-01-14 12:44:57 -0500

    Added Drag and Drop
    Added Drag and Drop functionality

---
+++ Client/wwwroot/js/playlistDragDrop.js
@@ -0,0 +1,35 @@
+﻿window.playlistDragDrop = {
+    instance: null,
+
+    initialize: function (elementId, dotNetHelper) {
+        const el = document.getElementById(elementId);
+        if (!el) return;
+
+        if (this.instance) {
+            this.instance.destroy();
+        }
+
+        this.instance = Sortable.create(el, {
+            animation: 150,
+            handle: '.drag-handle',
+            ghostClass: 'opacity-50',
+            dragClass: 'border-indigo-500',
+            onEnd: function (evt) {
+                dotNetHelper.invokeMethodAsync('OnSongReordered', evt.oldIndex, evt.newIndex);
+            }
+        });
+    },
+
+    destroy: function () {
+        if (this.instance) {
+            this.instance.destroy();
+            this.instance = null;
+        }
+    },
+
+    setEnabled: function (enabled) {
+        if (this.instance) {
+            this.instance.option('disabled', !enabled);
+        }
+    }
+};
