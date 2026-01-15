629dfcd91cca252585f1c958637529e720373fa5
joseph horvath <josephhorvath00@gmail.com>
2026-01-15 00:13:58 -0500

    Added test code for playlists
    playlist service, controller, and repository test code added

---
+++ music-manager-starter.Tests/IntegrationTests/PlaylistControllerTests.cs
@@ -0,0 +1,187 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using music_manager_starter.Server.Controllers;
+using music_manager_starter.Server.Services;
+using music_manager_starter.Shared;
+using System.Security.Claims;
+using Xunit;
+
+namespace music_manager_starter.Tests.IntegrationTests
+{
+    public class PlaylistControllerTests
+    {
+        private static PlaylistController CreateController(
+            Mock<IPlaylistService> serviceMock,
+            string? userId = "test-user")
+        {
+            var controller = new PlaylistController(serviceMock.Object);
+            var claims = new List<Claim>();
+            if (userId != null)
+            {
+                claims.Add(new Claim(ClaimTypes.Name, userId));
+            }
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(claims))
+                }
+            };
+            return controller;
+        }
+
+        [Fact]
+        public async Task Create_ValidName_ReturnsOkWithGuid()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var expectedId = Guid.NewGuid();
+            mockService
+                .Setup(s => s.CreatePlaylistAsync("My Playlist", "test-user"))
+                .ReturnsAsync(expectedId);
+
+            var controller = CreateController(mockService);
+            var result = await controller.Create("My Playlist");
+
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult!.Value.Should().Be(expectedId);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOkWithPlaylists()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlists = new List<PlaylistDto>
+            {
+                new PlaylistDto { Id = Guid.NewGuid(), Name = "Playlist A" },
+                new PlaylistDto { Id = Guid.NewGuid(), Name = "Playlist B" }
+            };
+            mockService
+                .Setup(s => s.GetAllPlaylistsAsync("test-user"))
+                .ReturnsAsync(playlists);
+
+            var controller = CreateController(mockService);
+            var result = await controller.GetAll();
+
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult!.Value.Should().BeEquivalentTo(playlists);
+        }
+
+        [Fact]
+        public async Task Get_ExistingPlaylist_ReturnsOkWithPlaylist()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+            var playlist = new PlaylistDto { Id = playlistId, Name = "Test Playlist" };
+            mockService
+                .Setup(s => s.GetPlaylistAsync(playlistId, "test-user"))
+                .ReturnsAsync(playlist);
+
+            var controller = CreateController(mockService);
+            var result = await controller.Get(playlistId);
+
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult!.Value.Should().BeEquivalentTo(playlist);
+        }
+
+        [Fact]
+        public async Task Rename_ValidRequest_ReturnsNoContent()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+
+            var controller = CreateController(mockService);
+            var result = await controller.Rename(playlistId, "New Name");
+
+            result.Should().BeOfType<NoContentResult>();
+            mockService.Verify(
+                s => s.RenamePlaylistAsync(playlistId, "New Name", "test-user"),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task Delete_ExistingPlaylist_ReturnsNoContent()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+
+            var controller = CreateController(mockService);
+            var result = await controller.Delete(playlistId);
+
+            result.Should().BeOfType<NoContentResult>();
+            mockService.Verify(
+                s => s.DeletePlaylistAsync(playlistId, "test-user"),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task AddSongs_ValidRequest_ReturnsNoContent()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+            var songIds = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+            var controller = CreateController(mockService);
+            var result = await controller.AddSongs(playlistId, songIds);
+
+            result.Should().BeOfType<NoContentResult>();
+            mockService.Verify(
+                s => s.AddSongsAsync(playlistId, songIds, "test-user"),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task RemoveSongs_ValidRequest_ReturnsNoContent()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+            var songIds = new List<Guid> { Guid.NewGuid() };
+
+            var controller = CreateController(mockService);
+            var result = await controller.RemoveSongs(playlistId, songIds);
+
+            result.Should().BeOfType<NoContentResult>();
+            mockService.Verify(
+                s => s.RemoveSongsAsync(playlistId, songIds, "test-user"),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task Reorder_ValidRequest_ReturnsNoContent()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+            var orderedIds = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+            var controller = CreateController(mockService);
+            var result = await controller.Reorder(playlistId, orderedIds);
+
+            result.Should().BeOfType<NoContentResult>();
+            mockService.Verify(
+                s => s.ReorderSongsAsync(playlistId, orderedIds, "test-user"),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task Export_CsvFormat_ReturnsOkWithCsv()
+        {
+            var mockService = new Mock<IPlaylistService>();
+            var playlistId = Guid.NewGuid();
+            var csvData = "Title,Artist,Album\nSong1,Artist1,Album1";
+            mockService
+                .Setup(s => s.ExportAsync(playlistId, "csv", "test-user"))
+                .ReturnsAsync(csvData);
+
+            var controller = CreateController(mockService);
+            var result = await controller.Export(playlistId, "csv");
+
+            var okResult = result.Result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            okResult!.Value.Should().Be(csvData);
+        }
+    }
+}
---
+++ music-manager-starter.Tests/UnitTests/Repositories/PlaylistRepositoryTests.cs.cs
@@ -0,0 +1,143 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using music_manager_starter.Data;
+using music_manager_starter.Data.Models;
+using music_manager_starter.Server.Repositories;
+using Xunit;
+
+namespace music_manager_starter.Tests.UnitTests.Repositories
+{
+    public class PlaylistRepositoryTests
+    {
+        private static DataDbContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            return new DataDbContext(options);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ExistingPlaylist_ReturnsPlaylist()
+        {
+            var context = CreateDbContext();
+            var playlistId = Guid.NewGuid();
+            var playlist = new Playlist
+            {
+                Id = playlistId,
+                Name = "Test",
+                CreatedByUserId = "user1",
+                PlaylistSongs = new List<PlaylistSong>()
+            };
+            context.Playlists.Add(playlist);
+            await context.SaveChangesAsync();
+
+            var repo = new PlaylistRepository(context);
+            var result = await repo.GetByIdAsync(playlistId, "user1");
+
+            result.Should().NotBeNull();
+            result!.Name.Should().Be("Test");
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_WrongUser_ReturnsNull()
+        {
+            var context = CreateDbContext();
+            var playlist = new Playlist
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                CreatedByUserId = "user1",
+                PlaylistSongs = new List<PlaylistSong>()
+            };
+            context.Playlists.Add(playlist);
+            await context.SaveChangesAsync();
+
+            var repo = new PlaylistRepository(context);
+            var result = await repo.GetByIdAsync(playlist.Id, "user2");
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task RemoveSongsAsync_RemovesAndReordersCorrectly()
+        {
+            var context = CreateDbContext();
+            var playlistId = Guid.NewGuid();
+            var song1Id = Guid.NewGuid();
+            var song2Id = Guid.NewGuid();
+            var song3Id = Guid.NewGuid();
+
+            var playlist = new Playlist
+            {
+                Id = playlistId,
+                Name = "Test",
+                CreatedByUserId = "user1"
+            };
+            context.Playlists.Add(playlist);
+
+            context.PlaylistSongs.AddRange(
+                new PlaylistSong { PlaylistId = playlistId, SongId = song1Id, OrderIndex = 0 },
+                new PlaylistSong { PlaylistId = playlistId, SongId = song2Id, OrderIndex = 1 },
+                new PlaylistSong { PlaylistId = playlistId, SongId = song3Id, OrderIndex = 2 }
+            );
+            await context.SaveChangesAsync();
+
+            var repo = new PlaylistRepository(context);
+            await repo.RemoveSongsAsync(playlistId, new List<Guid> { song2Id });
+            await repo.SaveChangesAsync();
+
+            var remaining = await context.PlaylistSongs
+                .Where(ps => ps.PlaylistId == playlistId)
+                .OrderBy(ps => ps.OrderIndex)
+                .ToListAsync();
+
+            remaining.Should().HaveCount(2);
+            remaining[0].SongId.Should().Be(song1Id);
+            remaining[0].OrderIndex.Should().Be(0);
+            remaining[1].SongId.Should().Be(song3Id);
+            remaining[1].OrderIndex.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task ReplaceSongsAsync_UpdatesOrderCorrectly()
+        {
+            var context = CreateDbContext();
+            var playlistId = Guid.NewGuid();
+            var song1Id = Guid.NewGuid();
+            var song2Id = Guid.NewGuid();
+
+            var playlist = new Playlist
+            {
+                Id = playlistId,
+                Name = "Test",
+                CreatedByUserId = "user1"
+            };
+            context.Playlists.Add(playlist);
+
+            context.PlaylistSongs.AddRange(
+                new PlaylistSong { PlaylistId = playlistId, SongId = song1Id, OrderIndex = 0 },
+                new PlaylistSong { PlaylistId = playlistId, SongId = song2Id, OrderIndex = 1 }
+            );
+            await context.SaveChangesAsync();
+
+            var repo = new PlaylistRepository(context);
+            var newOrder = new List<PlaylistSong>
+            {
+                new PlaylistSong { PlaylistId = playlistId, SongId = song2Id },
+                new PlaylistSong { PlaylistId = playlistId, SongId = song1Id }
+            };
+            await repo.ReplaceSongsAsync(playlistId, newOrder);
+
+            var reordered = await context.PlaylistSongs
+                .Where(ps => ps.PlaylistId == playlistId)
+                .OrderBy(ps => ps.OrderIndex)
+                .ToListAsync();
+
+            reordered[0].SongId.Should().Be(song2Id);
+            reordered[0].OrderIndex.Should().Be(0);
+            reordered[1].SongId.Should().Be(song1Id);
+            reordered[1].OrderIndex.Should().Be(1);
+        }
+    }
+}
